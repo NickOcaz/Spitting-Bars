dc6646c4fbf1572a2bab5339db4f60b2a6da62a7
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-29 14:21:51 +0000

    lyric_app installed

--- spitting_bars/settings.py
+++ spitting_bars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&x(#o%(kai-s01!j1*j=kd8*$w@hovn-c^z$ji6u-emi^mor&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lyric_app',
 ]
 
 MIDDLEWARE = [
