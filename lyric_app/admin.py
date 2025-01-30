c1e0da960848fd5e7384937077d470633bbe81d7
JeffChen <jeffchen1118@gmail.com>
2025-01-30 14:40:45 +0000

    added Allauth working on lyric_list.html

--- lyric_app/admin.py
+++ lyric_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Lyric
 
 
+
 # Register your models here.
 class LyricAdmin(admin.ModelAdmin):
     list_display = ('title', 'artist', 'status', 'updated_at')
