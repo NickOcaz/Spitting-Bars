780ef6364877b63c779dc63ac901a75bd3c128fc
JeffChen <jeffchen1118@gmail.com>
2025-01-30 11:40:36 +0000

    created empty genre_list.html

--- lyric_app/admin.py
+++ lyric_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Genre, Lyric
 # Register your models here.
+
+admin.site.register(Genre)
+
+admin.site.register(Lyric)
--- lyric_app/views.py
+++ lyric_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Genre, Lyric
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the lyric_app index.")
+class GenreList(generic.ListView):
+    queryset = Genre.objects.all()
+    template_name = 'lyric_app/genre_list.html'
+    
+
+class LyricList(generic.ListView):
+    model = Lyric
+    template_name = 'lyric_app/lyric_list.html'
+    
+def lyric_detail(request, pk):
+    lyric = Lyric.objects.get(pk=pk)
+    return render(request, 'lyric_app/lyric_detail.html', {'lyric': lyric})
