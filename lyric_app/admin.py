7f56aa9b381f807aeccd37f63f950f776455ceec
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-31 17:31:18 +0000

    fixed admin approval of publisehd pages

--- lyric_app/admin.py
+++ lyric_app/admin.py
@@ -3,25 +3,29 @@ from .models import Lyric
 from .models import PostApproval
 
 
-
-
 # Register your models here.
+
 class LyricAdmin(admin.ModelAdmin):
     list_display = ('title', 'artist', 'status', 'updated_at')
     search_fields = ('title', 'content')
     list_filter = ('status', 'updated_at')
+    actions = ['publish_lyrics', 'unpublish_lyrics']
 
-admin.site.register(Lyric)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(status=1)  # Assuming 'published' corresponds to 1
+
+    def publish_lyrics(self, request, queryset):
+        queryset.update(status=1)  # Assuming 'published' corresponds to 1
+        self.message_user(request, "Selected lyrics have been published.")
+
+    def unpublish_lyrics(self, request, queryset):
+        queryset.update(status=0)  # Assuming 'unpublished' corresponds to 0
+        self.message_user(request, "Selected lyrics have been unpublished.")
+
+    publish_lyrics.short_description = "Publish selected lyrics"
+    unpublish_lyrics.short_description = "Unpublish selected lyrics"
+
+admin.site.register(Lyric, LyricAdmin)
 
 
-class PostApprovalAdmin(admin.ModelAdmin):
-    list_display = ('get_post_title', 'approved_by', 'approved_at')
-    search_fields = ('post__title', 'approved_by__username')
-    list_filter = ('approved_at',)
-
-    def get_post_title(self, obj):
-        return obj.post.title
-    get_post_title.admin_order_field = 'post'  # Allows column order sorting
-    get_post_title.short_description = 'Post Title'  # Renames column head
-
-admin.site.register(PostApproval, PostApprovalAdmin)
